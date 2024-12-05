c919764ab6e0d84ebf7c3729dfa3ce837cabd353
editor2526 <jameson2526@gmail.com>
2024-12-06 06:23:01 +0700

    Make the text red #1

--- DigitalClockApp/Form1.Designer.cs
+++ DigitalClockApp/Form1.Designer.cs
@@ -52,6 +52,7 @@ namespace DigitalClockApp
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Crimson;
             this.Name = "Form1";
             this.Text = "Digital Clock Cunt";
             this.Load += new System.EventHandler(this.Form1_Load);
